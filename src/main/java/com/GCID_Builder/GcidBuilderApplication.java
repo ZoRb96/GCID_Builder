18e096a3ac699cff2ecda734b77d420f36406ebd
ZoRb96 <sauravbhagabati@gmail.com>
2021-07-20 00:52:07 +0530

    Initial commit

---
+++ src/main/java/com/GCID_Builder/GcidBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.GCID_Builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcidBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcidBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GCID_Builder/GcidBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GCID_Builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcidBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
